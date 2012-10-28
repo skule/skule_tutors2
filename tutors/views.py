424ca5c79813cf0f519fbb8c982a938f69b2b3b6
Oliver Liang <ollie.liang@gmail.com>
2012-10-28 19:28:12 -0400

    fixed profile creation bug that causes object_id error

--- tutors/views.py
+++ tutors/views.py
@@ -20,12 +20,13 @@ def TutorApplication(request, template = 'tutors/tutor_application.html'):
             user_data = user_creation_form.cleaned_data
             user = create_user(user_data[ 'email' ], user_data[ 'password1' ])
 
-            tutors_group = Group.objects.filter(name = 'Tutors')
-            if not tutors_group:
-                tutors_group = [ Group(name = 'Tutors').save() ]
+            try:
+                tutors_group = Group.objects.get(name = 'Tutors')
+            except Group.DoesNotExist:
+                tutors_group = Group.objects.create(name = 'Tutors')
 
             user.is_staff = False
-            user.groups.add(tutors_group[ 0 ])
+            user.groups.add(tutors_group)
 
             profile_data = profile_creation_form.cleaned_data
             user.first_name = profile_data[ 'first_name' ]
@@ -46,7 +47,9 @@ def TutorApplication(request, template = 'tutors/tutor_application.html'):
                 tutor.taught_courses.add(course)
             tutor.save()
 
-            messages.add_message(request, messages.INFO, 'Your application has been successfully received and is pending approval. Thank you for applying for Skule Tutors.')
+            messages.add_message(request, messages.INFO,
+                                 'Your application has been successfully received and is pending approval. Thank you '
+                                 'for applying for Skule Tutors.')
             return HttpResponseRedirect('/')
     else:
         user_creation_form = EmailUserCreationForm()
