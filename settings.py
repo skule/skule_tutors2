57b1b9ddf41ab80a396989eadcba3b382f6c5445
Oliver Liang <ollie.liang@gmail.com>
2012-10-28 21:03:54 -0400

    added heroku settings, one last fucking time

--- settings.py
+++ settings.py
@@ -33,7 +33,7 @@ TIME_ZONE = 'America/Toronto'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-ca'
+LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
 
@@ -58,7 +58,7 @@ MEDIA_URL = '/media'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -71,11 +71,11 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/'),
-    )
+# Put strings here, like "/home/html/static" or "C:/www/django/static".
+# Always use forward slashes, even on Windows.
+# Don't forget to use absolute paths, not relative paths.
+#os.path.join(os.path.dirname(__file__), 'static').replace('\\', '/'),
+)
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -86,7 +86,7 @@ STATICFILES_FINDERS = (
     )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '&%j49m@zbb5j7#(5ab&w8wczzuqb^j0x*u%uiqnw1-ys9_9uhc'
+SECRET_KEY = 'NxG6PeAtVG9n776UiHGqlsWOTR7aqVNBbtfEIhaIoeRSZQcYJv_EUchsZd5zltERoLooRu0O2OTcGFSH4pcAxZcVjKrYmPTHd'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
