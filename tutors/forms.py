38ce89590663896e2e6c8b2cb47707ee54c61d13
Oliver Liang <ollie.liang@gmail.com>
2012-11-04 01:06:52 -0400

    updated form display template

--- tutors/forms.py
+++ tutors/forms.py
@@ -13,6 +13,7 @@ class ApplicationForm(forms.Form):
     phone = forms.CharField(required = False)
 
     # TODO use chosen for the multi-select fieldhttp://harvesthq.github.com/chosen/
+    # or http://blog.abdelrahman.it/2010/jquery-autocomplete-search/
     taught_courses = forms.ModelMultipleChoiceField(queryset = Course.objects.all())
     rate = forms.DecimalField(decimal_places = 2, max_digits = 10)
     qualifications = forms.CharField(widget = forms.Textarea)
