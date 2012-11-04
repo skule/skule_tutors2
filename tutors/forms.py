afe2c19757b11672b852167557f85bbccd63c4f9
Oliver Liang <ollie.liang@gmail.com>
2012-11-04 01:12:45 -0400

    researching multi-select widgets

--- tutors/forms.py
+++ tutors/forms.py
@@ -12,8 +12,8 @@ class ApplicationForm(forms.Form):
     last_name = forms.CharField()
     phone = forms.CharField(required = False)
 
-    # TODO use chosen for the multi-select fieldhttp://harvesthq.github.com/chosen/
-    # or http://blog.abdelrahman.it/2010/jquery-autocomplete-search/
+    # TODO use chosen for the multi-select field http://harvesthq.github.com/chosen/
+    # or http://ivaynberg.github.com/select2/
     taught_courses = forms.ModelMultipleChoiceField(queryset = Course.objects.all())
     rate = forms.DecimalField(decimal_places = 2, max_digits = 10)
     qualifications = forms.CharField(widget = forms.Textarea)
