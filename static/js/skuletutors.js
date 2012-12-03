b5e1ff39e5c8d4b6a04f1f2f71ea1a20f1738a42
Oliver Liang <oliverliang@cogeco.ca>
2012-12-03 15:21:04 -0500

    removed formatting error from trademark icon, started javascript for collapsible qualification list

---
+++ static/js/skuletutors.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+        //collapsible management
+        $('.qualifications').collapsible({
+            defaultOpen: 'section1,section3',
+            cssClose: 'collapse-close',
+            cssOpen: 'collapse-open',
+        });
+    });
