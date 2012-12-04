025bbfc67269ba154ea7007f0adfe8eb6e5109f1
Oliver Liang <ollie.liang@gmail.com>
2012-12-03 19:13:20 -0500

    added admin logging for Tutor_delete_handler signal

--- tutors/models.py
+++ tutors/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from course_manage.models import Course
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from django.contrib.admin.models import LogEntry, DELETION
+from django.contrib.contenttypes.models import ContentType
 
 class Tutor(models.Model):
     # personal info
@@ -22,7 +24,7 @@ class Tutor(models.Model):
 
     def __unicode__(self):
         return self.name
-    
+
     def qualifications_as_list(self):
         return self.qualifications.split('\n')
 
@@ -34,3 +36,19 @@ def Tutor_delete_handler(sender, instance, **kwargs):
     user = instance.auth
     if not user.is_staff and not user.is_superuser:
         user.delete()
+
+        # add admin log entry for user deletion
+        delete_entries = LogEntry.objects.filter(content_type__pk = ContentType.objects.get_for_model(instance).pk,
+                                                 object_id = instance.pk, action_flag = DELETION)\
+        .order_by('-action_time')
+        # add log entry only if tutor is deleted via the admin interface
+        if delete_entries:
+            delete_entry = delete_entries[ 0 ]
+            LogEntry.objects.log_action(
+                user_id = delete_entry.user.pk,
+                content_type_id = ContentType.objects.get_for_model(user).pk,
+                object_id = user.pk,
+                object_repr = unicode(user.email),
+                action_flag = DELETION,
+                change_message = unicode('deleted user per Tutor_delete_handler signal')
+            )
