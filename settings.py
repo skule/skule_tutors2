3413b28c884a4848b3b20d6b5e21dbd11a9db82b
Oliver Liang <ollie.liang@gmail.com>
2012-12-22 21:01:02 -0500

    use postgres for local development use environmental variable for settings.DEBUG added initial migration for course_manage use syncdb, then manage.py migrate --fake on production server as per http://south.aeracode.org/wiki/ConvertingAnApp

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 # Django settings for skule_tutors2 project.
 
-DEBUG = True
+DEBUG = eval(os.environ.get('DJANGO_DEBUG', 'True'))
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -12,13 +12,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql',
         # 'sqlite3' or 'oracle'.
-        'NAME': 'database/database.db', # Or path to database file if using sqlite3.
-        'USER': '', # Not used with sqlite3.
-        'PASSWORD': '', # Not used with sqlite3.
-        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'tutors', # Or path to database file if using sqlite3.
+        'USER': 'tutors_dev', # Not used with sqlite3.
+        'PASSWORD': 'admin', # Not used with sqlite3.
+        'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432', # Set to empty string for default. Not used with sqlite3.
     }
 }
 
