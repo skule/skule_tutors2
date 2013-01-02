787ca7c9f70ab6641b40398325370300e73ec718
Oliver Liang <ollie.liang@gmail.com>
2013-01-01 22:17:56 -0500

    Don't display the staff associated Tutor profiles to non-superusers, ensure no staff accounts can be accidentally approved for display.

--- tutors/admin.py
+++ tutors/admin.py
@@ -16,16 +16,24 @@ class TutorAdmin(admin.ModelAdmin):
     readonly_fields = [ 'last_updated', 'auth' ]
     filter_horizontal = [ 'taught_courses' ]
 
+    def queryset(self, request):
+        '''
+        Don't display the staff associated Tutor profiles to non-superusers, ensure no staff accounts can be
+        accidentally approved for display.
+        '''
+        qs = super(TutorAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(auth__is_staff = False)
 
     def approveTutor(self, request, queryset):
         rows_updated = queryset.update(approved = True)
         if rows_updated == 1:
-            message_bit = "1 tutor was"
+            message_bit = "1 tutor was" # so much work to be grammatically correct
         else:
             message_bit = "%s tutors were" % rows_updated
         self.message_user(request, "%s successfully approved." % message_bit)
         # TODO add notification to tutor of his profile's status
-
     approveTutor.short_description = "Approve Selected Tutors"
 
     def disapproveTutor(self, request, queryset):
@@ -35,7 +43,6 @@ class TutorAdmin(admin.ModelAdmin):
         else:
             message_bit = "%s tutors were" % rows_updated
         self.message_user(request, "%s successfully disapproved." % message_bit)
-
     disapproveTutor.short_description = "Disapprove Selected Tutors"
 
 admin.site.register(Tutor, TutorAdmin)
