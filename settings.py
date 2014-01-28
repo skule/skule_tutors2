c07c5d44642c62313223505f854b77e5adc9e88c
Oliver Liang <oliver@engturtle.com>
2014-01-27 21:11:28 -0500

    Development Database Settings

--- settings.py
+++ settings.py
@@ -12,20 +12,18 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql',
-        # 'sqlite3' or 'oracle'.
-        'NAME': 'tutors', # Or path to database file if using sqlite3.
-        'USER': 'tutors_dev', # Not used with sqlite3.
-        'PASSWORD': 'admin', # Not used with sqlite3.
-        'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432', # Set to empty string for default. Not used with sqlite3.
-    }
-}
+DATABASES = {}
 
 if os.getenv('DATABASE_URL'):
     DATABASES[ 'default' ] = dj_database_url.config()
+else:
+    DATABASES[ 'default' ] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd7mgc0d7rai4lt',
+        'HOST': 'ec2-54-243-227-29.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'saybdxcpuwfnhd',
+        'PASSWORD': 'g-ZnCoTMb0orDk7VKjoBW4DnOJ'}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
